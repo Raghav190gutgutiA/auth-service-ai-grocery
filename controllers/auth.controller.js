c72c4a0712f90001b72091fa29b71edbd5953327
Raghav190gutgutiA <raghavgutgutia172@gmail.com>
2026-07-12 02:40:56 +0530

    minor fixes

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -94,6 +94,7 @@ exports.login = async (req, res) => {
 
     res.status(200).json({
       user,
+	  token:token
     });
 
   } catch (err) {
@@ -202,11 +203,7 @@ exports.logout = async (req, res) => {
 
   try {
 
-    res.clearCookie("token", {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-    });
+    res.clearCookie("token", cookieOptions);
 
     res.status(200).json({
       message: "Logged out successfully",
