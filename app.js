9d0c3418c01c03c968045e39f2462c0d4a2e6ab2
Raghav190gutgutiA <raghavgutgutia172@gmail.com>
2026-07-12 01:21:26 +0530

    cors

--- app.js
+++ app.js
@@ -16,20 +16,8 @@ const allowedOrigins = [
 app.use(cookieParser());
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-
-    if (
-      allowedOrigins.includes(origin) ||
-      origin.includes("vercel.app") ||
-      origin.includes("netlify.app")
-    ) {
-      return callback(null, true);
-    }
-
-    return callback(new Error("Not allowed by CORS"));
-  },
-  credentials: true
+  origin: true,
+  credentials: true,
 }));
 
 app.use(express.json());
